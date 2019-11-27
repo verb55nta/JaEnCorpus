cd506fe3835a48607d4b4f34c67b256860df864c
verb55 <verb55nta@gmail.com>
2019-11-27 22:48:18 +0900

    Updates

---
+++ precache-manifest.f6f19ed3fd742b5e435c7f420c1e379e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "709df1672db88c2cf329",
+    "url": "/JaEnCorpus/static/js/main.709df167.chunk.js"
+  },
+  {
+    "revision": "1a68e910e7ec7468353b",
+    "url": "/JaEnCorpus/static/js/1.1a68e910.chunk.js"
+  },
+  {
+    "revision": "d5ad40633bef558cac7c",
+    "url": "/JaEnCorpus/static/js/runtime~main.d5ad4063.js"
+  },
+  {
+    "revision": "4aa01fb91fbc32c37a385fe954aad226",
+    "url": "/JaEnCorpus/index.html"
+  }
+];
