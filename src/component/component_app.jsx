423f151b948123ca49400cc1cc224c2a790b83ef
verb55 <verb.storage.in.net@gmail.com>
2020-07-26 20:03:55 +0900

    add descriptions

--- src/component/component_app.jsx
+++ src/component/component_app.jsx
@@ -51,13 +51,39 @@ export default class App extends React.Component {
 
         let contents = props.started == 1 ? <Contents props={props} /> : <Text></Text>;
 
+        let description = <Text>
+            <p>これはSCoREコーパス（https://www.score-corpus.org/）にある日本語と英語の例文を表示するものです．</p>
+            <p>描画にはReact Nativeを使用しております．</p>
+            <p>日本語例文を表示した３秒後に英語例文が出力されます．</p>
+            <p>最初は英語例文が100個程度しか表示されず，フル機能が使えないので，Updateを押して全ての英語例文を取得してください．（取得に数十秒~数分かかります）</p>
+            <strong>使い方</strong>
+            <ul>
+                <li>Init...webページを初期化します．一度キャッシュされた英語例文が全てクリアされます．</li>
+                <li>Start...例文の描画を開始します．</li>
+                <li>Next...次の例文を描画します．</li>
+                <li>Prev...前の例文を描画します．</li>
+                <li>Update...SCoREの全ての例文を取得します．</li>
+            </ul>
+            <strong>Updateした後</strong>
+            <ul>
+                <li>Check...例文をチェックします．</li>
+                <li>Uncheck...例文のチェックを外します．</li>
+                <li>Clear All Checks...例文のチェックを全て外します．</li>
+                <li>Generate From Checked...チェックした例文のみのリストを表示します．</li>
+            </ul>
+
+        </Text>
+
         return <div>
             <Loading props={props} />
             <View style={styles.container}>
+                {init}
                 {start}
             </View>
             {contents}
-
+            <View style={styles.container}>
+                {description}
+            </View>
         </div>
 
 
